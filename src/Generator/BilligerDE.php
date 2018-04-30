cd30b2e6c915c9924633d4263c4dbb7c5130771d
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2018-04-30 13:11:51 +0200

    laravel 5.5 update

--- src/Generator/BilligerDE.php
+++ src/Generator/BilligerDE.php
@@ -95,7 +95,7 @@ class BilligerDE extends CSVPluginGenerator
         $this->elasticExportPropertyHelper = pluginApp(ElasticExportPropertyHelper::class);
         
         $settings = $this->arrayHelper->buildMapFromObjectList($formatSettings, 'key', 'value');
-        $this->filtrationService = pluginApp(FiltrationService::class, [$settings, $filter]);
+		$this->filtrationService = pluginApp(FiltrationService::class, ['settings' => $settings, 'filterSettings' => $filter]);
         
         $this->elasticExportStockHelper->setAdditionalStockInformation($settings);
 
--- src/ResultField/BilligerDE.php
+++ src/ResultField/BilligerDE.php
@@ -49,7 +49,9 @@ class BilligerDE extends ResultFields
     {
         $settings = $this->arrayHelper->buildMapFromObjectList($formatSettings, 'key', 'value');
 
-        $this->setOrderByList(['item.id', ElasticSearch::SORTING_ORDER_ASC]);
+		$this->setOrderByList([
+			'path' => 'item.id',
+			'order' => ElasticSearch::SORTING_ORDER_ASC]);
 
         $reference = $settings->get('referrerId') ? $settings->get('referrerId') : self::BILLIGER_DE;
 
